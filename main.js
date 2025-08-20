c6985592c7af7ad092113e074af594c9deb9af66
sasaki-kaito-git <sasaki-kaito@ooo-unchain-ooo.xyz>
2025-08-20 19:01:27 +0900

    js に要素を作成

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 `use strict`;
 // todoの入力欄を取得
-const textInput = document.getElementById('text-input');
+const textInput = document.getElementById("text-input");
 
 // todoのリストを取得
-const todoList = document.getElementById('todo-list')
+const todoList = document.getElementById("todo-list");
 
 // todoの入力欄に対して、keydownイベントを登録
-textInput.addEventListener('keydown', e => {
-    // 入力されたテキストを取得し、前後の空白を削除
-    const text = textInput.value.trim();
-// 入力欄が空でないか、Enterキーが押されたかを確認
-    if (text === '' || e.key !== 'Enter') {
-        return;
-    }
-    console.log('Todoが入力された');
-});
+textInput.addEventListener("keydown", (e) => {
+  // 入力されたテキストを取得し、前後の空白を削除
+  const text = textInput.value.trim();
+  // 入力欄が空でないか、Enterキーが押されたかを確認
+  if (text === "" || e.key !== "Enter") {
+    return;
+  }
+  console.log("Todoが入力された");
 
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+
+  li.classListadd("list-item");
+  span.textContent = text;
+});
